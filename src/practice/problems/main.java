6604c3c49fa1539d8ef94581a81af25112f50a89
Brandon Hach <58790036+bamboosmoothies@users.noreply.github.com>
2022-05-15 13:27:36 -0400

    *added reverse String method

--- src/practice/problems/main.java
+++ src/practice/problems/main.java
@@ -14,7 +14,39 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //REVERSE YOUR WORDS!
+        String r = reverse("crapdosis");
+        System.out.println(r);
+        
+        System.out.println(reverse("chocolate"));
+        System.out.println(reverse("A Block of Cheese"));
+        System.out.println(reverse("racecar"));
+        
+    }
+    
+    //reverse String method 
+    /**
+     * 
+     * @param word
+     * @return reverse String
+     */
+    public static String reverse(String s){
+        //Declare and Initalize Array
+        char[] letters = new char[s.length()];
+        
+        //Reverse for-loop and store reverse letter to Array
+        int letterIndex = 0;
+        for(int i = s.length()-1; i >= 0; i--){   
+            letters[letterIndex] = s.charAt(i);
+            letterIndex++;
+        }
+        
+        //for-loop to store to String to return
+        String reverse = "";
+        for(int i = 0; i < s.length(); i++){
+            reverse += letters[i];
+        }
+        return reverse;
     }
     
 }
